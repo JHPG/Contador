7fe642277cf7e51537d619e7acad5ffd512feee0
Jorge Henrique P. Garcia <j.hrique@hotmail.com>
2015-02-19 08:53:11 -0200

    comment ...

--- ContaPessoas/FirstViewController.m
+++ ContaPessoas/FirstViewController.m
@@ -3,8 +3,8 @@
 //  ContaPessoas
 //
 //  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
-//
+//  Copyright (c) 2015 Jorge Henrique. All rights reserved.
+// ....................
 
 #import "FirstViewController.h"
 #import "Contador.h"
